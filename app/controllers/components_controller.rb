1584a638d685f5daade0eb9126dc755f7dde2cd7
railsenjoyer <railsenjoyer@gmail.com>
2023-03-15 13:07:45 +0300

    added create-view form

--- app/controllers/components_controller.rb
+++ app/controllers/components_controller.rb
@@ -1,18 +1,42 @@
 class ComponentsController < ApplicationController
+
   def create 
-    Component.create(
+    component = Component.create(
       title: params[:component][:title],
-      body: params[:component][:body],
+      country: params[:component][:country],
+      brand: params[:component][:brand],
+      market_date: params[:component][:market_date],
+      size: params[:component][:size],
+      connectors: params[:component][:connectors],
+      guarantee: params[:component][:guarantee],
+      complete_set: params[:component][:complete_set],
+      rating: params[:component][:rating],
+      weight: params[:component][:weight],
+      price: params[:component][:price],
+      availability: params[:component][:availability],
+      description: params[:component][:description],
       user_id: params[:component][:user_id]
     )
-    redirect_to '/'
+
+    redirect_to component_path(component)
   end
 
   def update 
     @component = Component.find(params[:id])
     @component.update(
       title: params[:component][:title],
-      body: params[:component][:body],
+      country: params[:component][:country],
+      brand: params[:component][:brand],
+      market_date: params[:component][:market_date],
+      size: params[:component][:size],
+      connectors: params[:component][:connectors],
+      guarantee: params[:component][:guarantee],
+      complete_set: params[:component][:complete_set],
+      rating: params[:component][:rating],
+      weight: params[:component][:weight],
+      price: params[:component][:price],
+      availability: params[:component][:availability],
+      description: params[:component][:description],
       user_id: params[:component][:user_id]
     )
   end
@@ -24,5 +48,12 @@ class ComponentsController < ApplicationController
 
   def show
     @component = Component.find(params[:id])
+  end
+
+  def index
+    @components = Component.all
+  end
+
+  def new
   end
 end

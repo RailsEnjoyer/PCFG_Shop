463edf2309a717c439c26f8cc7a8a094c02f47e9
railsenjoyer <railsenjoyer@gmail.com>
2023-03-16 14:15:20 +0300

    added main page & navigation

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  root to: 'components#index'
+  root to: 'main#index'
   
   resources :components
+  get '/cart', to: 'main#cart'
+  get '/delivery', to: 'main#delivery'
+  get '/support', to: 'main#support'
+  get '/about', to: 'main#about'
 end

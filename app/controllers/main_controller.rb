5b348bdc52473c6cd57aac027404ca1b1051b233
railsenjoyer <railsenjoyer@gmail.com>
2023-03-16 15:00:26 +0300

    changed links appearance

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,7 +1,2 @@
 class MainController < ApplicationController
-    def index 
-    end
-
-    def about
-    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
   root to: 'main#index'
   
   resources :components
+
+  get '/main', to: 'main#index'
   get '/cart', to: 'main#cart'
-  get '/delivery', to: 'main#delivery'
+  get '/news', to: 'main#news'
   get '/support', to: 'main#support'
   get '/about', to: 'main#about'
+  get '/configurator', to: 'main#configurator'
 end

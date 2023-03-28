35b40a169c3e37d73cf828e3fd64e60bb696235f
railsenjoyer <railsenjoyer@gmail.com>
2023-03-28 20:38:13 +0300

    styled catalogue page

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,3 +2,6 @@
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
+//= link bg/star.png
+//= link bg/half-star.png
+//= link bg/empty-star.png
